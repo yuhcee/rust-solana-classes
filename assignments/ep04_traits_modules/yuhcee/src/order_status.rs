b1799e6d70fdad44fbcf3adeeffbb1bcd0b41ff9
yuhcee <egbouchenna001@gmail.com>
2025-07-10 17:25:48 +0100

    Refactor: Remove print_status function from OrderStatus enum

--- assignments/ep04_traits_modules/yuhcee/src/order_status.rs
+++ assignments/ep04_traits_modules/yuhcee/src/order_status.rs
@@ -1,14 +1,7 @@
-
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub enum OrderStatus {
     Pending,
     Shipped,
     Delivered,
-}
-
-pub fn print_status(status: OrderStatus) {
-    match status {
-        OrderStatus::Pending => println!("Order is Pending"),
-        OrderStatus::Shipped => println!("Order is Shipped"),
-        OrderStatus::Delivered => println!("Order is Delivered"),
-    }
+    Cancelled,
 }
